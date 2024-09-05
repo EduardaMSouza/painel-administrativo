67a5f5fb07494b6434f8ca1df46bb404e032ec46
Eduarda <eduardasouza111213@gmail.com>
2024-09-05 05:19:05 -0300

    Adding a link to the register page

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,7 +1,7 @@
-// import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import Styles from "./Login.module.scss"
+import Styles from "./Login.module.scss";
 
 interface FormData {
   email: string;
@@ -15,18 +15,18 @@ export default function Login() {
     formState: { errors, isSubmitting },
   } = useForm<FormData>();
 
- 
   const navigate = useNavigate();
+
   async function authenticate(formData: FormData) {
     try {
       const response = await fetch(`${process.env.REACT_APP_HOST}/auth/login`, {
         method: "POST",
         headers: {
-          "Content-Type": "Application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
-      console.log(response)
+      console.log(response);
       const data = await response.json();
 
       if (!response.ok) {
@@ -43,47 +43,50 @@ export default function Login() {
 
   return (
     <>
-    <div className={Styles.container_login}>
-      <form onSubmit={handleSubmit((data) => authenticate(data))}>
-      <label htmlFor="email">Email:</label>
-        <div className={Styles.container_individual}>
-        <input
-          type="email"
-          {...register("email", {
-            required: "O email é obrigatório",
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "Insira um email válido",
-            },
-          })}
-        />
-        {errors.email && <span>{errors.email.message}</span>}
-        </div>
-        <label htmlFor="password">Senha:</label>
+      <div className={Styles.container_login}>
+        <form onSubmit={handleSubmit((data) => authenticate(data))}>
+          <label htmlFor="email">Email:</label>
           <div className={Styles.container_individual}>
-          <input
-           type="password"
-           {...register("password", {
+            <input
+              type="email"
+              {...register("email", {
+                required: "O email é obrigatório",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Insira um email válido",
+                },
+              })}
+            />
+            {errors.email && <span>{errors.email.message}</span>}
+          </div>
+          <label htmlFor="password">Senha:</label>
+          <div className={Styles.container_individual}>
+            <input
+              type="password"
+              {...register("password", {
                 required: "A senha é obrigatória",
                 minLength: {
-                value: 6,
-                message: "A senha deve ter no mínimo 6 caracteres",
-            },
-              maxLength: {
-                value: 20,
-                message: "A senha deve ter no máximo 20 caracteres",
-            },
-          })}
-        />
-          {errors.password && <span>{errors.password.message}</span>}
-        </div>
+                  value: 6,
+                  message: "A senha deve ter no mínimo 6 caracteres",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "A senha deve ter no máximo 20 caracteres",
+                },
+              })}
+            />
+            {errors.password && <span>{errors.password.message}</span>}
+          </div>
           
-        <button type="submit" disabled={isSubmitting}>
-          {isSubmitting ? "Carregando..." : "Entrar"}
-        </button>
-      </form>
-    </div>
+          <button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? "Carregando..." : "Entrar"}
+          </button>
+          <div className={Styles.register_link}>
+          <p>Não tem uma conta? <Link to="/cadastro">Cadastre-se</Link></p>
+        </div>
+        </form>
+        
+      </div>
     </>
-    
   );
 }
