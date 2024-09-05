499102643a6e52af4caf33efb78cfe01be984891
Josias Carneiro <josiasjt3@gmail.com>
2024-09-05 02:14:43 -0300

    Adicionando o cadastro e a estilização do cadastro.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginClient from './pages/Login';
+import Cadastro from './components/Cadastro/Cadastro';
 
 function App() {
   return (
